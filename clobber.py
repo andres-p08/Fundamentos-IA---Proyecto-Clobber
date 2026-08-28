59109075517fe0811cfd51a1eb8135a7e207e6b8
Andrés Peña <a.peaegaa@uandresbello.edu>
2026-08-27 23:01:46 -0400

    Add files via upload

--- clobber.py
+++ clobber.py
@@ -3,7 +3,6 @@ class Clobber(Board):
     __jugadorA:  str
     __jugadorB:  str
 
-    VALIDOS = [(1,0),(-1,0),(0,1),(0,-1)]
 
     def __init__(self, tamano, jugadorA: str = "A", jugadorB: str = "B"):
         super().__init__(tamano)
@@ -13,30 +12,56 @@ class Clobber(Board):
     def pedirTamano(self):
         self.tamano = int(input("Tamaño n del tablero: "))
 
-    def jugadaA(self, f: int, c: int, x: int, y: int) -> bool:
+    def jugada(self, f: int, c: int, x: int, y: int) -> bool:
         if self[f, c] == "A":
-            if self.valid_moveA(f, c, x, y):
+            if self.valid_move(f, c, x, y, self[f, c]):
                 self[x, y] = self.__jugadorA
                 self[f, c] = "·"
                 return True
-            else:
-                return False
-
-    def jugadaB(self, f: int, c: int, x: int, y: int) -> bool:
-        if self[f, c] == "B":
-            if self.valid_moveB(x, y):
+            
+        elif self[f, c] == "B":
+            if self.valid_move(f, c, x, y, self[f, c]):
                 self[x, y] = self.__jugadorB
                 self[f, c] = "·"
                 return True
             else:
                 return False
 
-    def valid_moveA(self, f, c, x, y):
+    def valid_move(self, f, c, x, y, color):
         if [f-x, c-y] == [1,0] or [f-x, c-y] == [-1,0] or [f-x, c-y] == [0,1] or [f-x, c-y] == [0,-1]:
-            return self[x, y] == "B"
+            if color == "A":
+                return self[x, y] == "B"
+            elif color == "B":
+                return self[x, y] == "A"
         else: 
             return False
 
-    def valid_moveB(self, f, c, x, y):
-        if [f-x, c-y] == [1,0] or [f-x, c-y] == [-1,0] or [f-x, c-y] == [0,1] or [f-x, c-y] == [0,-1]:
-            return self[x, y] == "A"
+    def hayMovimientos(self):
+        tamano = len(self)
+        for f in range(1, tamano + 1):
+            for c in range(1, tamano + 1):
+                ficha_actual = self[f, c]
+
+                #Ignoramos los espacios vacios
+                if ficha_actual not in ("A", "B"):
+                    continue
+
+                # Revisamos las 4 direcciones validas
+                validos = [(f-1, c), (f+1, c), (f, c-1), (f, c+1)]
+
+                for x, y in validos:
+                    try:
+                        # Revisamos jugadas validas para el jugador A y B
+                        if self[f, c] == "A":
+                            if self[x, y] == "B":
+                                return True
+
+                        if self[f, c] == "B":
+                            if self[x, y] == "A":
+                                return True
+
+                    except LookupError:
+                            pass
+        return False
+                    
+
--- game.py
+++ game.py
@@ -2,10 +2,41 @@ from clobber import Clobber
 if __name__ == "__main__":
     tamano = int(input("Tamaño n del tablero: "))
     tablero = Clobber(tamano)
+    turno = "A"
+    ganador = ""
+    hay_movimientos = True
     while True:
         print(tablero)
-        print("Turno del jugador A: \nEscoja la ficha")
-        tablero.jugadaA(int(input("Fila: ")), int(input("Columna: ")), int(input("\nHaga el movimiento\nFila: ")), int(input("Columna: ")))
-        print(tablero)
-        print("Turno del jugador B: \nEscoja la ficha")
-        tablero.jugadaB(int(input("Fila: ")), int(input("Columna: ")), int(input("\nHaga el movimiento\nFila: ")), int(input("Columna: ")))
+        print(f"Turno del jugador {turno}: ")
+
+        # Verificamos si hay movimientos disponibles
+        if not tablero.hayMovimientos():
+            print(f"El jugador {turno} no tiene mas movimientos")
+            break
+        print("Escoja la ficha")
+        f = int(input("Fila: "))
+        c = int(input("Columna: "))
+
+        # Validamos si escogio una ficha valida
+        try:
+            ficha_seleccionada = tablero[f, c]
+        except LookupError:
+            print("Coordenadas fuera de rango. Intente de nuevo.\n")
+            continue
+        if ficha_seleccionada != turno:
+            print(f"Solo puedes mover las fichas '{turno}'. Intente de nuevo.\n")
+            continue
+        x = int(input("\nHaga el movimiento\nFila: "))
+        y = int(input("Columna: "))
+
+        # Se realiza la jugada y pasa el siguiente turno
+        if tablero.jugada(f, c, x, y):
+            ganador = turno
+            
+            turno = "B" if turno == "A" else "A"
+        else:
+            print("Movimiento inválido. Intente de nuevo.\n")
+
+# Se muestra el resultado final
+print(tablero)
+print(f"Gana el jugador {ganador}")
